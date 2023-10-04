e91abfc63ae2808116b1cfe21a9639074959839c
Ws96I2i <145530738+Ws96I2i@users.noreply.github.com>
2023-10-03 22:57:46 -0700

    Update dns_test.zsh
    删除失效的DNS

--- dns_test.zsh
+++ dns_test.zsh
@@ -11,10 +11,12 @@
 # https://www.threads.net/@root_challenger
 # https://www.tumblr.com/blog/rootchallenger
 # https://mastodon.social/@Root_Challenger
-# Revision: 1.0.0
+# Revision: 1.0.1
 ###############################################################
 #README
 #本来是写bash的，后来改成zsh，因为Mac自带的bash版本太老了,这样对于移植到其他的Mac不利,所以走zsh.减少出错几率
+#Change log
+#删除了无效的DNS服务
 
 if [[ $EUID -ne 0 ]]; then
    echo "请使用sudo权限运行此脚本." 
@@ -33,7 +35,6 @@ dns_groups=(
     "Cloudflare" "1.1.1.1 1.0.0.1"              # 用于加速和保护网站的DNS
     "OpenDNS" "208.67.222.222 208.67.220.220"  # 由Cisco提供的DNS服务
     "Quad9" "9.9.9.9 149.112.112.112"          # 集成了安全功能的DNS
-    "DNS.Watch" "84.200.69.80 84.200.70.40"    # 德国的公共DNS
     "Verisign" "64.6.64.6 64.6.65.6"            # 提供DDoS保护和速度加速的DNS
     "Comodo Secure DNS" "8.26.56.26 8.20.247.20" # 安全的DNS服务
     "UncensoredDNS" "91.239.100.100 89.233.43.71" # 不审查的DNS
@@ -42,8 +43,8 @@ dns_groups=(
     "Neustar" "156.154.70.1 156.154.71.1"         # 提供DDoS保护和速度加速
     "AdGuard DNS" "94.140.14.14 94.140.15.15"     # 广告拦截功能
     "AdGuard 广告和成人内容过滤" "94.140.14.15 94.140.15.16" # 广告拦截、成人内容过滤
-    "CleanBrowsing" "185.228.168.9 185.228.169.9"   # 提供家长控制和成人内容过滤功能
-    "Alternate DNS" "198.101.242.72 23.253.163.53"  # 广告拦截和恶意软件保护功能的DNS
+    "CleanBrowsing" "185.228.168.168 185.228.169.168"#阻止訪問所有成人、色情和露骨網站。它還會阻止用於繞過過濾器的代理程式和 VPN 網域。混合內容網站（如 Reddit）也被封鎖。Google、Bing 和 Youtube 設定為安全模式。惡意和網路釣魚域被封鎖。
+    "Alternate DNS" "76.76.19.19 76.223.122.150"  # 广告拦截和恶意软件保护功能的DNS
 )
 
 echo "\n测试中，请稍候...\n"
